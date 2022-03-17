c57027b6990e77c4dd6a63d359ca34b17ad001d6
Javier Gutiérrez Bravo <jgbravo.developer@gmail.com>
2022-03-17 21:28:16 +0100

    Initial commit

---
+++ app/src/main/java/com/jgbravo/actiasystemsmobile/ActiaSystemsMobileApp.kt
@@ -0,0 +1,7 @@
+package com.jgbravo.actiasystemsmobile
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ActiaSystemsMobileApp : Application()
---
+++ app/src/main/java/com/jgbravo/actiasystemsmobile/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jgbravo.actiasystemsmobile.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jgbravo.actiasystemsmobile.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
